4eb56ffffedfdee42810f3b7f2c63b68530020ad
chenyongzhouking <64853785+chenyongzhouking@users.noreply.github.com>
2022-08-20 17:30:32 +0200

    adesso si può importare da file e modificato punto di partenza e sistemato il problema della funzione checkSide

--- MyProject.cpp
+++ MyProject.cpp
@@ -21,7 +21,7 @@ struct element {
 };
 
 const std::vector<element> SceneToLoad = {
-	{"models/ceiling.obj", "textures/EOBRB01.png"},
+	{"models/ceiling.obj", "textures/EOBFLT12.png"},
 	{"models/doors.obj", "textures/EBBRD01.png"},
 	{"models/door1.obj","textures/EOBDR02.png"},
 	{"models/door2.obj","textures/EOBDR02.png"},
@@ -36,7 +36,7 @@ const std::vector<element> SceneToLoad = {
 	{"models/floor.obj","textures/EOBSLM04.png"},
 	{"models/goldKey.obj","textures/GoldKey.png"},
 	{"models/copperKey.obj","textures/CopperKey.png"},
-	{"models/walls.obj", "textures/EOBRB01.png"},
+	{"models/wall.obj", "textures/EOBRB01.png"},
 	{"models/winText.obj","textures/white.png"},
 	{"models/keyText.obj","textures/white.png"},
 	{"models/keyText1.obj","textures/white.png"},
@@ -49,34 +49,8 @@ const std::vector<element> SceneToLoad = {
 };
 
 
+std::string vettore[24][24];
 
-
-std::vector<std::vector<std::string>> vettore = {
-{"*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"," "," ","*","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"," "," ","*","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*","*","*","*"," "," "," ","*","*"," "," ","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*","*","*","*"," ","*"," "," "," ","*"," ","*","*","*","*","*"},
-{"*","*","*","*","*","*"," ","*","*","*","*","*"," ","*","*"," "," ","*"," "," ","*","*","*","*"},
-{"*","*","*","*","*","*"," ","*","*","*"," "," "," "," "," ","*","*","*"," "," ","*","*","*","*"},
-{"*","*","*","*","*","*"," ","*","*","*"," ","*","*","*"," ","*"," ","*"," ","*","*","*","*","*"},
-{"*","*","*","*","*","*"," ","*","*"," "," ","*"," "," "," ","*"," ","*"," ","*","*","*","*","*"},
-{"*","*","*","*","*","*"," "," "," "," "," "," ","*"," "," ","*"," "," "," ","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*"," "," ","*","*"," ","*"," "," ","*"," ","*","*","*","*","*"},
-{"*","*","*","*","*","*"," "," "," ","*","*"," ","*"," ","*"," "," ","*"," ","*","*","*","*","*"},
-{"*","*","*","*"," "," "," "," "," "," "," "," "," "," ","*"," ","*"," "," "," ","*"," ","*","*"},
-{"*","*","*","*"," ","*"," "," "," ","*","*"," ","*","*"," "," ","*","*"," ","*"," "," "," ","*"},
-{"*","*","*","*"," ","*","*","*","*","*","*","*"," "," "," "," ","*"," "," "," ","*"," ","*","*"},
-{"*","*"," "," "," "," "," "," "," "," "," "," "," ","*","*"," ","*"," "," "," ","*"," ","*","*"},
-{"*","*"," ","*"," ","*","*","*","*","*","*","*","*","*","*"," "," ","*"," ","*"," "," ","*","*"},
-{"*","*"," ","*"," "," "," "," "," "," "," "," "," "," "," ","*"," "," "," "," "," ","*","*","*"},
-{"*","*"," ","*"," "," "," ","*","*","*"," "," "," ","*","*","*","*","*"," ","*","*","*","*","*"},
-{"*","*"," ","*","*","*","*","*"," ","*"," ","*","*"," "," "," "," "," "," ","*","*","*","*","*"},
-{"*"," "," "," ","*","*","*"," "," "," "," "," "," "," ","*","*","*","*","*","*","*","*","*","*"},
-{"*"," "," "," "," "," "," "," ","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"},
-{"*"," "," "," ","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"},
-{"*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"}
-};
 
 //PROBLEMI DA RISOLVERE
 //NON RIESCO A IMPORTARE IL FILE MAP.TXT E FARE OPERAZIONI SU DI ESSA
@@ -196,7 +170,7 @@ protected:
 	Texture TDoorText5;
 	DescriptorSet DSDoorText5;
 
-
+	//door4 (12.4,0.8,3.9)
 	std::vector<Model> ModelToLoad = { MCeiling,MDoors,MDoor1,MDoor2,MDoor3,MDoor4,MDoor5,MLever5,MLever3,MLever1,MGoldHole,MCopperHole,MFloor,MGoldKey,MCopperKey,MWalls,MWinText,MKeyText,MKeyText1,MDoorText1,MDoorText2,MDoorText3,MDoorText4,MDoorText5 };
 	std::vector<Texture> TextureToLoad = { TCeiling,TDoors,TDoor1,TDoor2,TDoor3,TDoor4,TDoor5,TLever5,TLever3,TLever1,TGoldHole,TCopperHole,TFloor,TGoldKey,TCopperKey,TWalls,TWinText,TKeyText,TKeyText1, TDoorText1,TDoorText2,TDoorText3,TDoorText4,TDoorText5 };
 	std::vector<DescriptorSet> DescriptorToLoad = { DSCeiling,DSDoors,DSDoor1,DSDoor2,DSDoor3,DSDoor4,DSDoor5,DSLever5,DSLever3,DSLever1,DSGoldHole,DSCopperHole,DSFloor,DSGoldKey,DSCopperKey,DSWalls,DSWinText,DSKeyText,DSKeyText1,DSDoorText1,DSDoorText2,DSDoorText3,DSDoorText4,DSDoorText5 };
@@ -222,31 +196,61 @@ protected:
 			});
 
 		P1.init(this, "shaders/vert.spv", "shaders/frag.spv", { &DSL1 });
+		int k = 0;
 
 		for (int i = 0; i < 24; i++) {
-			ModelToLoad[i].init(this, SceneToLoad[i].ObjFile);
-			TextureToLoad[i].init(this, SceneToLoad[i].TextureFile);
+
+			ModelToLoad[i].init(this, SceneToLoad[i].ObjFile); //k
+			TextureToLoad[i].init(this, SceneToLoad[i].TextureFile); //k
 			DescriptorToLoad[i].init(this, &DSL1, {
 						{0, UNIFORM, sizeof(UniformBufferObject), nullptr},
 						{1, TEXTURE, 0, &TextureToLoad[i]}
 				});
+			if (strcmp(SceneToLoad[i].ObjFile, "models/doorText4.obj") != 0) {
+				k++;
+			}
+		}
+		printf("kappo %d \n", k);
+		int i = 0;
+		k = 0;
+		int j = 0; 
+		std::string str;
+		std::ifstream file("models/map.txt");
+		std::getline(file, str);
+		while (std::getline(file, str))
+		{
+			while (str[i] != '}')
+			{
+				if (str[i] == ' ')
+				{
+					vettore[j][k] = " ";
+					k++;
+				}
+				if (str[i] == '*')
+				{
+					vettore[j][k] = "*";
+					k++;
+				}
+				i++;
+			}
+			i = 0;
+			j++;
+			k =0 ;
 		}
 
-		FILE *f;
-		char vec[100];
-		f = fopen("models/map.txt","r");
-		while (fgets(vec, 100, f) != NULL)
-		{
-			printf("%s ", vec);
-		}
+
 	}
 
 	// Here you destroy all the objects you created!		
 	void localCleanup() {
+		int k = 0;
 		for (int i = 0; i < 24; i++) {
 			DescriptorToLoad[i].cleanup();
-			ModelToLoad[i].cleanup();
-			TextureToLoad[i].cleanup();
+			ModelToLoad[i].cleanup();//k
+			TextureToLoad[i].cleanup();//k
+			if (strcmp(SceneToLoad[i].ObjFile, "models/doorText4.obj") != 0) {
+				k++;
+			}
 		}
 		P1.cleanup();
 		DSL1.cleanup();
@@ -259,19 +263,22 @@ protected:
 
 		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
 			P1.graphicsPipeline);
-
+		int k = 0;
 		for (int i = 0; i < 24; i++) {
-			VkBuffer vertexBuffers[] = { ModelToLoad[i].vertexBuffer };
+			VkBuffer vertexBuffers[] = { ModelToLoad[i].vertexBuffer }; //k
 			VkDeviceSize offsets[] = { 0 };
 			vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
-			vkCmdBindIndexBuffer(commandBuffer, ModelToLoad[i].indexBuffer, 0,
+			vkCmdBindIndexBuffer(commandBuffer, ModelToLoad[i].indexBuffer, 0,    //k
 				VK_INDEX_TYPE_UINT32);
 			vkCmdBindDescriptorSets(commandBuffer,
 				VK_PIPELINE_BIND_POINT_GRAPHICS,
 				P1.pipelineLayout, 0, 1, &DescriptorToLoad[i].descriptorSets[currentImage],
 				0, nullptr);
 			vkCmdDrawIndexed(commandBuffer,
-				static_cast<uint32_t>(ModelToLoad[i].indices.size()), 1, 0, 0, 0);
+				static_cast<uint32_t>(ModelToLoad[i].indices.size()), 1, 0, 0, 0);  //k
+			if (strcmp(SceneToLoad[i].ObjFile, "models/doorText4.obj") != 0) {
+				k++;
+			}
 		}
 	}
 
@@ -282,11 +289,11 @@ protected:
 	}
 
 
-	void checkSide(glm::vec4 *blockLimit, glm::vec4 *recentlyWalls, glm::vec2 *matrixCoo, int direction) {
+	void checkSide(glm::vec4 *blockLimit, glm::vec4 *recentlyWalls, int *matrixCoo, int direction) {
 
-		static glm::vec2 coo = *matrixCoo;
-		static glm::vec4 limit = *blockLimit;
-		static glm::vec4 recent = *recentlyWalls;
+
+		glm::vec4 limit = *blockLimit;
+		glm::vec4 recent = *recentlyWalls;
 		float value;
 		int column = 0;
 		int row = 0;
@@ -312,8 +319,8 @@ protected:
 				row = 1;
 			}
 		}
-
-		if (vettore[coo[1]+column][coo[0]+row] == "*")
+		//printf("\n limit 0 dentro la funzione %f ",blockLimit[0]);
+		if (vettore[matrixCoo[1]+column][matrixCoo[0]+row] == "*")
 		{
 			if (recent[direction] == 0)
 			{
@@ -351,27 +358,23 @@ protected:
 
 		static float debounce = time;
 
-		std::ifstream map("models/map.txt", std::ios::ate | std::ios::binary);
-		if (!map.is_open()) {
-			throw std::runtime_error("failed to open file!");
-		}
 
 		// Updates unifoms for the objects
 		static glm::mat3 CamDir = glm::mat3(1.0f);
 		static glm::vec3 CamPos = glm::vec3(0.0f, 0.5f, 2.5f);
-		static glm::vec3 RobotPos = glm::vec3(1, 0, 3);
+		static glm::vec3 RobotPos = glm::vec3(4, 0, 1);//4,0,1   1,0,3
 		glm::vec3 RobotCamDeltaPos = glm::vec3(0.0f, 0.335f, -0.0f);
 		static float lookYaw = 0.0;
-		static float lookPitch = 270.0;
-		static float lookRoll = 0.0;
-		static glm::vec4 blockLimit = glm::vec4(1.5, 0.5, 3.5, 2.5);
-		static glm::vec4 recentlyWalls = glm::vec4(0, 0, 0, 0);
-		static glm::vec2 matrixCoo = glm::vec2(7, 12);
+		static float lookPitch = 270.0; // 0 
+		static float lookRoll = 0.0; 
+		static glm::vec4 blockLimit = glm::vec4(4.5, 3.5, 1.5, 0.5); //4.5, 3.5, 1.5, 0.5    1.5, 0.5, 3.5, 2.5
+		static glm::vec4 recentlyWalls = glm::vec4(0, 0, 0, 0);//destra, sinistra, sotto, sopra
+		static int matrixCoo[2] = { 10, 10 };//10,10   7,12
 		static glm::vec3 oldPos;
 
 		oldPos = RobotPos;
 
-
+		
 		UniformBufferObject ubo{};
 
 		if (glfwGetKey(window, GLFW_KEY_LEFT)) {
@@ -422,6 +425,8 @@ protected:
 		}
 
 
+
+
 		static bool dCheck1 = false;
 		static bool dCheck2 = false;
 		static bool dCheck3 = false;
@@ -430,9 +435,12 @@ protected:
 		static bool kCheck1 = false;
 		static bool kCheck2 = false;
 
+		
 		if (blockLimit[0] < RobotPos[0])
 		{
 			matrixCoo[0]++;
+			//printf("la mia coordinata x %f ", matrixCoo[0]);
+
 			if (vettore[matrixCoo[1]][matrixCoo[0]] == "*" || blockLimit[0] == 3.5 && RobotPos.z <= 3.4 && RobotPos.z >= 2.6 && dCheck1 == false || blockLimit[0] == 6.5 && RobotPos.z <= 8.5 && RobotPos.z >= 7.6 && dCheck2 == false)
 			{
 				RobotPos = oldPos;
@@ -452,22 +460,28 @@ protected:
 				else {
 					//l'if forse si può togliere considera blocco di dimensione minore di 1, considera caso muro dietro pero adesso vado al blocco davanti
 					blockLimit[0]++;
+					//printf("aumento normale %f ", blockLimit[0]);
+
 				}
 
 				printf("%f ", blockLimit[1]);
 				printf("%f ", blockLimit[0]);
+				checkSide(&blockLimit,&recentlyWalls,matrixCoo,2);
+				//printf("dopo la funzione %f ", blockLimit[0]);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 3);
 
-				checkSide(&blockLimit,&recentlyWalls,&matrixCoo,2);
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 3);
 
 				printf("%f ", blockLimit[3]);
 				printf("%f \n", blockLimit[2]);
+				//printf("block limit 0 alla fine %f \n", blockLimit[0]);
+
 			}
 		}
 
 
 		if (blockLimit[1] > RobotPos[0])
 		{
+
 			matrixCoo[0]--;
 			if (vettore[matrixCoo[1]][matrixCoo[0]] == "*" || blockLimit[0] == 4.5 && RobotPos.z <= 3.4 && RobotPos.z >= 2.6 && dCheck1 == false)
 			{
@@ -480,22 +494,25 @@ protected:
 				//if yes and i have a wall same resut else plus 0.2
 				//in this case olny for block 2 and 3 
 				printf("sto andando dietro ");
-				blockLimit[0] = blockLimit[1];
-				recentlyWalls[0] = 0;
 
 				if (vettore[matrixCoo[1]][matrixCoo[0] - 1] == "*")
 				{
+					blockLimit[0] = blockLimit[1];
 					blockLimit[1] = blockLimit[1] - 0.9;
 					recentlyWalls[1] = 1;
+					recentlyWalls[0] = 0;
 				}
 				else {
+					blockLimit[0] = blockLimit[1];
+					recentlyWalls[0] = 0;
 					blockLimit[1] --;
 				}
 				printf("%f ", blockLimit[1]);
 				printf("%f ", blockLimit[0]);
-				
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 2);
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 3);
+
+
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 2);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 3);
 
 				printf("%f ", blockLimit[3]);
 				printf("%f \n", blockLimit[2]);
@@ -507,6 +524,7 @@ protected:
 		//verso giu
 		if (blockLimit[2] < RobotPos[2])
 		{
+
 			matrixCoo[1]++;
 			if (vettore[matrixCoo[1]][matrixCoo[0]] == "*" || blockLimit[2] == -2.5 && RobotPos.x <= 4.5 && RobotPos.x >= 3.6 && dCheck5 == false || blockLimit[2] == 2.5 && RobotPos.x <= 9.5 && RobotPos.x >= 8.6 && dCheck3 == false || blockLimit[2] == 3.5 && RobotPos.x <= 12.5 && RobotPos.x >= 11.5 && dCheck4 == false)
 			{
@@ -515,19 +533,24 @@ protected:
 			}
 			else {
 				printf("sto andando giu ");
-				blockLimit[3] = blockLimit[2];
-				recentlyWalls[3] = 0;
+
 				if (vettore[matrixCoo[1] + 1][matrixCoo[0]] == "*")
 				{
+					blockLimit[3] = blockLimit[2];
 					blockLimit[2] = blockLimit[2] + 0.9;
 					recentlyWalls[2] = 1;
+					recentlyWalls[3] = 0;
+
 				}
 				else {
+					blockLimit[3] = blockLimit[2];
+					recentlyWalls[3] = 0;
 					blockLimit[2] ++;
 				}
 
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 0);
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 1);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 0);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 1);
+
 
 				printf("%f ", blockLimit[1]);
 				printf("%f ", blockLimit[0]);
@@ -540,6 +563,7 @@ protected:
 		//verso su
 		if (blockLimit[3] > RobotPos[2])
 		{
+
 			matrixCoo[1]--;
 			if (vettore[matrixCoo[1]][matrixCoo[0]] == "*" || blockLimit[3] == -1.5 && RobotPos.x <= 4.4 && RobotPos.x >= 3.5 && dCheck5 == false || blockLimit[3] == 3.5 && RobotPos.x <= 9.4 && RobotPos.x >= 8.5 && dCheck3 == false || blockLimit[3] == 4.5 && RobotPos.x <= 12.5 && RobotPos.x >= 11.5 && dCheck4 == false)
 			{
@@ -548,19 +572,22 @@ protected:
 			}
 			else {
 				printf("sto andando su ");
-				blockLimit[2] = blockLimit[3];
-				recentlyWalls[2] = 0;
+
 				if (vettore[matrixCoo[1] - 1][matrixCoo[0]] == "*")
 				{
+					blockLimit[2] = blockLimit[3];
 					blockLimit[3] = blockLimit[3] - 0.9;
 					recentlyWalls[3] = 1;
+					recentlyWalls[2] = 0;
 				}
 				else {
+					blockLimit[2] = blockLimit[3];
+					recentlyWalls[2] = 0;
 					blockLimit[3] --;
 				}
 
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 0);
-				checkSide(&blockLimit, &recentlyWalls, &matrixCoo, 1);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 0);
+				checkSide(&blockLimit, &recentlyWalls, matrixCoo, 1);
 
 				printf("%f ", blockLimit[1]);
 				printf("%f ", blockLimit[0]);
@@ -672,7 +699,7 @@ protected:
 			}
 			if (i == 19) {
 				if (dCheck1) {
-					ubo.model = glm::translate(glm::mat4(1.0), glm::vec3(0, 1, 0));
+					ubo.model = glm::translate(glm::mat4(1.0), glm::vec3(0, 1, 0))*glm::translate(glm::mat4(1.0), glm::vec3(2.5, 0.8, -2.2))*glm::translate(glm::mat4(1.0), glm::vec3(-12.4, -0.8, -3.9));
 				}
 			}
 			if (i == 20) {
